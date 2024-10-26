7997a4aa64b2b17f5e17eff3eda9710a5002a693
ebert-l <entwickler@leon-ebert.de>
2024-10-26 19:14:18 +0200

    Header pattern- added the submenus

--- patterns/header.php
+++ patterns/header.php
@@ -10,24 +10,112 @@
 ?>
 <!-- wp:group {"align":"wide","className":"has-inter-font-family","backgroundColor":"weiss","layout":{"type":"flex","justifyContent":"right","flexWrap":"wrap"}} -->
 <div class="wp-block-group alignwide has-inter-font-family has-weiss-background-color has-background" id="header"><!-- wp:group {"layout":{"type":"flex"}} -->
-	<div id="logo" class="wp-block-group"><!-- wp:image {"id":27,"sizeSlug":"full","linkDestination":"none"} -->
-	<figure class="wp-block-image size-full"><img src="<?php echo get_parent_theme_file_uri( 'assets/images/avh_logo_small.webp');?>" alt="" class="wp-image-27" /></figure>
-	<!-- /wp:image --></div>
-	<!-- /wp:group -->
-	
-	<!-- wp:separator {"className":"menu-divider","style":{"spacing":{"margin":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"}}},"backgroundColor":"schwarz"} -->
-	<hr class="wp-block-separator has-text-color has-schwarz-color has-alpha-channel-opacity has-schwarz-background-color has-background menu-divider" style="margin-top:var(--wp--preset--spacing--20);margin-bottom:var(--wp--preset--spacing--20)"/>
-	<!-- /wp:separator -->
-	
-	<!-- wp:navigation {"ref":4,"overlayBackgroundColor":"wappen","overlayTextColor":"contrast"} /-->
-	
-	<!-- wp:separator {"className":"menu-divider","style":{"spacing":{"margin":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"}}},"backgroundColor":"schwarz"} -->
-	<hr class="wp-block-separator has-text-color has-schwarz-color has-alpha-channel-opacity has-schwarz-background-color has-background menu-divider" style="margin-top:var(--wp--preset--spacing--20);margin-bottom:var(--wp--preset--spacing--20)"/>
-	<!-- /wp:separator -->
-	
-	<!-- wp:buttons -->
-	<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-fill"} -->
-	<div class="wp-block-button is-style-fill"><a class="wp-block-button__link wp-element-button">Intern</a></div>
-	<!-- /wp:button --></div>
-	<!-- /wp:buttons --></div>
-	<!-- /wp:group -->
+<div id="logo" class="wp-block-group"><!-- wp:image {"lightbox":{"enabled":false},"id":27,"sizeSlug":"full","linkDestination":"custom"} -->
+<figure class="wp-block-image size-full"><a href="/"><img src="http://localhost/avh/wp-content/themes/av-huette/assets/images/avh_logo_small.webp" alt="" class="wp-image-27"/></a></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group -->
+
+<!-- wp:separator {"className":"menu-divider","style":{"spacing":{"margin":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"}}},"backgroundColor":"schwarz"} -->
+<hr class="wp-block-separator has-text-color has-schwarz-color has-alpha-channel-opacity has-schwarz-background-color has-background menu-divider" style="margin-top:var(--wp--preset--spacing--20);margin-bottom:var(--wp--preset--spacing--20)"/>
+<!-- /wp:separator -->
+
+<!-- wp:navigation {"ref":4,"overlayBackgroundColor":"wappen","overlayTextColor":"contrast","className":"avh-header-menu"} /-->
+
+<!-- wp:separator {"className":"menu-divider","style":{"spacing":{"margin":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"}}},"backgroundColor":"schwarz"} -->
+<hr class="wp-block-separator has-text-color has-schwarz-color has-alpha-channel-opacity has-schwarz-background-color has-background menu-divider" style="margin-top:var(--wp--preset--spacing--20);margin-bottom:var(--wp--preset--spacing--20)"/>
+<!-- /wp:separator -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-fill"} -->
+<div class="wp-block-button is-style-fill"><a class="wp-block-button__link wp-element-button">Intern</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"avh-header-submenu hideonfrontend","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div id="SubMenu1" class="wp-block-group avh-header-submenu hideonfrontend"><!-- wp:group {"className":"avh-header-submenu-info","layout":{"type":"constrained"}} -->
+<div class="wp-block-group avh-header-submenu-info"><!-- wp:paragraph {"className":"avh-header-submenu-infotitle"} -->
+<p class="avh-header-submenu-infotitle">Die Aktivitas</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"className":"avh-header-submenu-infotitle"} -->
+<p class="avh-header-submenu-infotitle">Wir sind junge Studierende<br>aller Fachrichtungen der Berliner Hochschulen und organisieren uns.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:navigation {"ref":4,"overlayMenu":"never","className":"avh-header-submenu-menu","layout":{"type":"flex","orientation":"vertical"}} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"avh-header-submenu-image","layout":{"type":"constrained"}} -->
+<div class="wp-block-group avh-header-submenu-image"><!-- wp:image {"id":62,"sizeSlug":"full","linkDestination":"none","className":"avh-header-submenu-image-image"} -->
+<figure class="wp-block-image size-full avh-header-submenu-image-image"><img src="http://localhost/avh/wp-content/uploads/2024/10/wp6406961.jpg" alt="" class="wp-image-62"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"avh-header-submenu hideonfrontend","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div id="SubMenu2" class="wp-block-group avh-header-submenu hideonfrontend"><!-- wp:group {"className":"avh-header-submenu-info","layout":{"type":"constrained"}} -->
+<div class="wp-block-group avh-header-submenu-info"><!-- wp:paragraph {"className":"avh-header-submenu-infotitle"} -->
+<p class="avh-header-submenu-infotitle">Die Aktivitas</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"className":"avh-header-submenu-infotitle"} -->
+<p class="avh-header-submenu-infotitle">Wir sind junge Studierende<br>aller Fachrichtungen der Berliner Hochschulen und organisieren uns.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:navigation {"ref":4,"overlayMenu":"never","className":"avh-header-submenu-menu","layout":{"type":"flex","orientation":"vertical"}} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"avh-header-submenu-image","layout":{"type":"constrained"}} -->
+<div class="wp-block-group avh-header-submenu-image"><!-- wp:image {"id":62,"sizeSlug":"full","linkDestination":"none","className":"avh-header-submenu-image-image"} -->
+<figure class="wp-block-image size-full avh-header-submenu-image-image"><img src="http://localhost/avh/wp-content/uploads/2024/10/wp6406961.jpg" alt="" class="wp-image-62"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"avh-header-submenu hideonfrontend","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div id="SubMenu3" class="wp-block-group avh-header-submenu hideonfrontend"><!-- wp:group {"className":"avh-header-submenu-info","layout":{"type":"constrained"}} -->
+<div class="wp-block-group avh-header-submenu-info"><!-- wp:paragraph {"className":"avh-header-submenu-infotitle"} -->
+<p class="avh-header-submenu-infotitle">Die Aktivitas</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"className":"avh-header-submenu-infotitle"} -->
+<p class="avh-header-submenu-infotitle">Wir sind junge Studierende<br>aller Fachrichtungen der Berliner Hochschulen und organisieren uns.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:navigation {"ref":4,"overlayMenu":"never","className":"avh-header-submenu-menu","layout":{"type":"flex","orientation":"vertical"}} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"avh-header-submenu-image","layout":{"type":"constrained"}} -->
+<div class="wp-block-group avh-header-submenu-image"><!-- wp:image {"id":62,"sizeSlug":"full","linkDestination":"none","className":"avh-header-submenu-image-image"} -->
+<figure class="wp-block-image size-full avh-header-submenu-image-image"><img src="http://localhost/avh/wp-content/uploads/2024/10/wp6406961.jpg" alt="" class="wp-image-62"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"avh-header-submenu hideonfrontend","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div id="SubMenu4" class="wp-block-group avh-header-submenu hideonfrontend"><!-- wp:group {"className":"avh-header-submenu-info","layout":{"type":"constrained"}} -->
+<div class="wp-block-group avh-header-submenu-info"><!-- wp:paragraph {"className":"avh-header-submenu-infotitle"} -->
+<p class="avh-header-submenu-infotitle">Die Aktivitas</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"className":"avh-header-submenu-infotitle"} -->
+<p class="avh-header-submenu-infotitle">Wir sind junge Studierende<br>aller Fachrichtungen der Berliner Hochschulen und organisieren uns.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:navigation {"ref":4,"overlayMenu":"never","className":"avh-header-submenu-menu","layout":{"type":"flex","orientation":"vertical"}} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"avh-header-submenu-image","layout":{"type":"constrained"}} -->
+<div class="wp-block-group avh-header-submenu-image"><!-- wp:image {"id":62,"sizeSlug":"full","linkDestination":"none","className":"avh-header-submenu-image-image"} -->
+<figure class="wp-block-image size-full avh-header-submenu-image-image"><img src="http://localhost/avh/wp-content/uploads/2024/10/wp6406961.jpg" alt="" class="wp-image-62"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
